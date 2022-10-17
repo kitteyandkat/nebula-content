af4987e4bf3b6a9dd28bc14e6558ae6a6703aa08
kitteyandkat <57220112+kitteyandkat@users.noreply.github.com>
2022-10-17 17:06:08 -0400

    my commit message

---
+++ 1-variables/assignments/1-variable-hw.js
@@ -0,0 +1,37 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+let name = "Frank"
+let age = 15
+let color = 'grey'
+let length = '183cm'
+const birthYear = 2007
+const sex = 'male'
+
+{
+    console.log(name)
+    console.log(age)
+    console.log(color)
+    console.log(length)
+    console.log(birthYear)
+    console.log(sex)
+}
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const brand = 'subaru'
+const model = 'WRX'
+let ExteriorColor = 'red'
+const InteriorColor = 'black'
+const Year = 2004
+let Odometer = 115094
+{
+    console.log(brand)
+    console.log(model)
+    console.log(ExteriorColor)
+    console.log(InteriorColor)
+    console.log(Year)
+    console.log(Odometer)
+}
