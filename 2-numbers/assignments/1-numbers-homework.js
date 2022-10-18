ae3ed996bbca981873a0ef2fd84c87f1fada4627
kitteyandkat <57220112+kitteyandkat@users.noreply.github.com>
2022-10-18 01:45:40 -0400

    my commit message

---
+++ 2-numbers/assignments/1-numbers-homework.js
@@ -0,0 +1,76 @@
+
+// Remeber to console.log() to debug your code and check your solutions!
+
+
+// Declare a variable and assign it to a numeric value
+// Half that value and re-assign it to the previous variable 
+let sum = 69
+sum/=2
+console.log(sum)
+
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+let x = -52
+let y = -74
+console.log(`x:${x}, y:${y}`)
+let xy=x-y
+console.log(xy)
+
+// Create three numbers, assign them to variables and multiply them together 
+let b = 7
+let c = 15
+let d = 72
+d*=c
+d*=b
+console.log(d)
+
+// Create two even numbers, assign them to variables and divide one from the other 
+let e = 42
+let f = 6
+e/=f
+console.log(e)
+
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+let g = 65
+let h = 43
+g%h
+console.log(g)
+
+
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+let i = Math.min(10,20,30,40,50)
+console.log(i)
+
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+let j = Math.max(-1,-2,-3,-4,-5 )
+console.log(j)
+
+// Create a variable and find it's remainder, if divided by 2
+let k = 78425
+k%2
+
+// find the square root of 144 
+Math.sqrt(144)
+
+// find the value of 3 to the power of 7 
+Math.pow(3,7)
+
+// find the value of 73.2 rounded 
+Math.round(73.2)
+
+// find the rounded-down value of 9.9 
+Math.floor(9.9)
+
+// find the absolute value of -1231
+Math.abs(-1231)
+
+{
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(e)
+    console.log(f)
+    console.log(g)
+    console.log(h)
+    console.log(i)
+    console.log(j)
+}
