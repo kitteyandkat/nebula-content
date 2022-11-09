7a7538244bbc2b0cea03964eb5ad45e8b1b246df
kitteyandkat <57220112+kitteyandkat@users.noreply.github.com>
2022-11-09 18:09:55 -0500

    functions assignment

---
+++ 7-functions/assignments/function-assignment.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+function solution(str){
+  
+
+    let answer = '';
+    for (let i of str) {
+        answer = i + answer; 
+      }
+    return answer;
+      
+    }
+
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+
+function repeatStr (n, s) {
+    let str = ''
+    for (let i = 0; i<n; i++ ){
+      str+=s;
+      }
+     return str;
+   }
+
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
