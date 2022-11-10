cad0c6b799b43d70d7f17b01ab34dc04e535dedf
kitteyandkat <57220112+kitteyandkat@users.noreply.github.com>
2022-11-09 19:53:53 -0500

    objects assignment

--- 8-objects/assignments/1-object-assignment.js
+++ 8-objects/assignments/1-object-assignment.js
@@ -1,23 +1,36 @@
 // 1.   Create an object for your dream vehicle
 //      Give it a minimum of 6 key-value pairs
 //      This object should contain many datatypes (string, number, boolean, null, array)
-
+let car = {
+    Model: "Macan",
+    Make: "Porsche",
+    Turbo: true,
+    InsuredDrivers: ['Sarah', 'Lukas', 'Cynthia'],
+    Year: 2022,
+    TorqueVectoring: true,
+};
 
 // 2.   Using dot-notation Log to the console 3 properties from the previous object
+console.log(car.Model)
+console.log(car.InsuredDrivers)
+console.log(car.Year)
+
 
 
 // 3.   Using bracket-notation Log to the console 3 properties from the previous object
-
+console.log(car['Make'])
+console.log(car['Turbo'])
+console.log(car['TorqueVectoring'])
 
 // 4.   Try and log a property that doesn't already exist - what output do we get?
-
+console.log(car.color)
 
 // 5.   Add a new key-value pair to the vehicle. 
-
+car.color = 'Black'
 
 // 6.   Using bracket-notation update a property on the vehicle. 
-
-
+car.InsuredDrivers[1] = 'Abraham'
+console.log(car.InsuredDrivers)
 // 7.   Using dot-notation update a property on the vehicle. 
 
 
