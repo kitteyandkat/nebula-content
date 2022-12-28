68bf011dc88dbb75d4d6a72326022d72f859bd14
Sarah Johnson <kitteyandkat@aim.com>
2022-12-28 11:33:53 -0500

    repo updates

--- 12-classes/assignments/classes.js
+++ 12-classes/assignments/classes.js
@@ -56,3 +56,14 @@ class shoes {
 console.log(shoe1.shoeStats())
 console.log(shoe2.shoeStats())
 console.log(shoe3.shoeStats())
+
+/* 
+Given the following class that can create shapes, write an extension which will calculate the Surface Area and Volume of a given shape. 
+Note, one class extension will likely not be able to handle all shapes, you'll have to extend shape for each unique shape
+Log to the console a sentnece about your shape:
+`Given a cylander of 2 height and 2 width. It will have a volume of 6.28 and a surface area of 18.84.` 
+*/
+
+// Extend 4 classes off of shapes
+// Rectangle(A=bh), Triangle(A=1/2bh), Circle(A=πr2) and Trapezoid(A=1/2h(b1+b2))
+// Have methods to find the areas of the differnt shapes
